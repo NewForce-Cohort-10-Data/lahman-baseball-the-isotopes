f365a96e24a0f1e0e1e85e3dc9ec98b8c076a623
Ava <ava.melzak@gmail.com>
2025-02-06 15:02:04 -0500

    finished question 4

---
+++ question4.sql
@@ -0,0 +1,20 @@
+-- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
+
+SELECT DISTINCT playerid,
+	CASE
+	WHEN pos = 'OF' THEN 'Outfield'
+	WHEN pos = 'SS' OR pos = '1B' OR pos = '2B' OR pos = '3B' THEN 'Infield'
+	WHEN pos = 'P' OR pos = 'C' THEN 'Battery'
+	END AS position
+FROM fielding;
+
+SELECT
+	CASE
+	WHEN pos = 'OF' THEN 'Outfield'
+	WHEN pos = 'SS' OR pos = '1B' OR pos = '2B' OR pos = '3B' THEN 'Infield'
+	WHEN pos = 'P' OR pos = 'C' THEN 'Battery'
+	END AS position,
+	SUM(po) AS putouts
+FROM fielding
+WHERE yearid = '2016'
+GROUP BY position;
